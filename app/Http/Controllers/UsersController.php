7623d9713d5ef114c41398fcde7e5a4535a8d1ea
Senior_Paul <samwel.massawe1111@gmail.com>
2025-03-28 15:32:33 +0300

    controler :   app/Http/Controllers/UsersController.php User Moder	modified:   app/Models/User.php

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User; 
+
+class UsersControler extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        return view('list', ['users' => $users]);
+    }
+
+    public function create()
+    {
+        $user = User::all();
+        return view('form', ['user' => $user, 'action' => 'create', 'actionUrl' => 'store']);
+    }
+    public function store(Request $request)
+    {
+        $validationData = $request->validate([
+            'name' => 'required|string|max:255',
+            'age' => 'required|integer|min:0',
+        ]);
+
+        user::create($validationData);
+
+        return redirect('/')->with('success', 'User created successfully.');
+    }
+
+    public function edit(int $id)
+    {
+        $user = User::findOrFail($id);
+        return view('form', ['user' => $user, 'action' => 'update', 'actionUrl' => 'update/' . $id]);
+    }
+
+    public function update(Request $request, int $id)
+    {
+        $validationData = $request->validate([
+            'name' => 'required|string|max:255',
+            'age' => 'required|integer|min:0',
+        ]);
+
+        $user = User::findOrFail($id);
+        $user->update($validationData);
+
+        $user->name = $request->input('name');
+        $user->age = $request->input('age');
+        $user->save();
+
+        return redirect('/')->with('success', 'User updated successfully.');
+    }
+
+    public function delete(int $id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return redirect('/')->with('success', 'User deleted successfully.');
+    }
+}
