b2c0736baed07ab21256732392fdc969b4c6fb12
Senior_Paul <samwel.massawe1111@gmail.com>
2025-03-28 14:56:34 +0300

    form view:   resources/views/form.blade.php

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -21,5 +21,24 @@
                     </div>
                 @enderror
             </div>
+
+            <div class="form-group">
+                <label for="age">User Age</label>
+                <br>
+                <input id="age"
+                        name="age"
+                        type="text"
+                        value="{{ old('age' ?? $user->age) }}"
+                        class="form-control @error('age') is-invalid @enderror"
+                        placeholder="Enter your age">
+                @error('age')
+                    <div class="alert alert-dander">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+
         </form>
     </div>
+@endsection
